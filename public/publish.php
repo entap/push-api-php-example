1e37e96ea9507ae74dd8045f1b7f7b3b3f321119
ii masamitsu <ii@entap.co.jp>
2021-02-04 11:15:37 +0900

    パスを調整

--- public/publish.php
+++ public/publish.php
@@ -6,14 +6,17 @@ use Minishlink\WebPush\WebPush;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$publicKeyPath = __DIR__ . '/../keys/public_key.txt';
+$privateKeyPath = __DIR__ . '/../keys/private_key.txt';
+
 // application/json
 $subscription = json_decode(file_get_contents('php://input'), true);
 
 $auth = [
     'VAPID' => [
-        'subject' => 'https://github.com/entap',
-        'publicKey' => file_get_contents(__DIR__ . '/../keys/public_key.txt'), // don't forget that your public key also lives in app.js
-        'privateKey' => file_get_contents(__DIR__ . '/../keys/private_key.txt'), // in the real world, this would be in a secret file
+        'subject' => 'https://github.com/entap/push-api-php-example',
+        'publicKey' => file_get_contents($publicKeyPath), // don't forget that your public key also lives in app.js
+        'privateKey' => file_get_contents($privateKeyPath), // in the real world, this would be in a secret file
     ],
 ];
 
